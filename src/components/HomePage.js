479cc7dded1c2e1c82a30cc5363a7ed1632c0612
Neeraja Gadi <neerajarao2898@gmail.com>
2024-03-28 09:42:44 +0530

    HomePage added

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className='homepage'>
+       
+        <button>SHOW segment</button>
+
+    </div>
+  )
+}
+
+export default HomePage
